b8b2bc481af27fe39bb9858b19348651dc46e9a1
Mukund Visavadiya <mukundvisa1307@gmail.com>
2025-10-07 22:40:16 -0700

    second commit protfolio

--- index.php
+++ index.php
@@ -1,4 +1,48 @@
- <?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Portfolio</title>
+    <meta name="description" content="Mukund Visavadiya - Full-Stack Developer specializing in WordPress, custom themes, plugins, PHP, HTML/CSS, and jQuery. Crafting dynamic, high-performance websites.">
+    <meta name="keywords" content="Mukund Visavadiya, Full-Stack Developer, WordPress Developer, PHP Developer, Custom WordPress Theme, Custom WordPress Plugin, HTML, CSS, jQuery, Web Development, Next.js, React, Frontend, Backend">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/favicon.png" rel="apple-touch-icon">
+
+    <!-- Fonts -->
+    <link href="assets/font/font.css" rel="stylesheet">
+
+    <!-- CSS File -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/css/aos.css" rel="stylesheet">
+    <link href="assets/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/css/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+
+</head>
+
+<body class="index-page">
+
+    <header id="header" class="header d-flex flex-column justify-content-center">
+
+        <i class="header-toggle d-xl-none fa-solid fa-bars"></i>
+
+        <nav id="navmenu" class="navmenu">
+            <ul>
+                <li><a href="#hero" class="active"><i class="fa-solid fa-house"></i><span>Home</span></a></li>
+                <li><a href="#about"><i class="fa-solid fa-user"></i><span>About</span></a></li>
+                <li><a href="#resume"><i class="fa-solid fa-file-lines"></i><span>Resume</span></a></li>
+                <li><a href="#portfolio"><i class="fa-solid fa-globe"></i><span>Project</span></a></li>
+                <li><a href="#services"><i class="fa-solid fa-layer-group"></i><span>Services</span></a></li>
+                <li><a href="#contact"><i class="fa-regular fa-envelope"></i><span>Contact</span></a></li>
+            </ul>
+        </nav>
+
+    </header>
 
  <main class="main">
    <!-- Hero Section -->
@@ -137,12 +181,41 @@
            <div class="swiper-slide"><img src="assets/img/google-cloud.png" alt="google-cloud"></div>
          </div>
        </div>
-
      </div>
-
    </section>
 
 
  </main>
 
- <?php include 'footer.php'; ?>
+<footer id="footer" class="footer position-relative light-background">
+    <div class="container">
+        <div class="copyright">
+            <span>Copyright <?php echo date("Y"); ?></span> <strong class="px-1 sitename">Mukund Visavadiya</strong> <span>All Rights
+                Reserved</span>
+        </div>
+    </div>
+</footer>
+
+<!-- Scroll Top -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="fa-solid fa-arrow-up"></i></a>
+
+<!-- Preloader -->
+<div id="preloader"></div>
+
+<!-- JS File -->
+<script src="assets/js/aos.js"></script>
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/fontawesome.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/typed.umd.js"></script>
+<script src="assets/js/purecounter_vanilla.js"></script>
+<script src="assets/js/noframework.waypoints.js"></script>
+<script src="assets/js/glightbox.min.js"></script>
+<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="assets/js/swiper-bundle.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
